70f86a568a8bab6d69375e3d9388880f3349f571
xavir <xaviranik.me@gmail.com>
2020-12-09 20:02:58 +0600

    add: views directory

---
+++ src/views/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    id="main"
+    class="grid grid-cols-12 min-h-screen h-screen overflow-y-hidden"
+  >
+    <Sidebar />
+    <HomeContent />
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar";
+import HomeContent from "@/components/HomeContent";
+
+export default {
+  name: "Main",
+  components: {
+    Sidebar,
+    HomeContent,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Watch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen bg-red-700"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
